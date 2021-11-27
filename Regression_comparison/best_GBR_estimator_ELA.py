bcac4081594b17999c84ea94f8a95fc72990aaad
Edgar-La <laraae2018@licifug.ugto.mx>
2021-11-26 20:55:15 -0600

    GBR added

---
+++ Regression_comparison/best_GBR_estimator_ELA.py
@@ -0,0 +1,64 @@
+#Importamos las librerias a utilizar
+import os; os.system('clear')
+import pandas as pd
+import numpy as np
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import GridSearchCV, KFold
+import time
+from get_optim_metrics_ELA import get_optim_metrics1_ELA
+
+def best_SVR_estimator_ELA(dataset_name, id2save_metrics = 'DatasetMetrics',
+                           estimator_ = None, iter = 5, k_folds= 2, parameters = None):
+    
+    #Guardamos el tiempo en una variable para contar el tiempo que le toma correr
+    startTime = time.perf_counter()
+    
+    #Leemos el dataset y lo convertimos en datos manipulables
+    df = pd.read_csv(dataset_name)
+    n_cols = len(df.columns)
+    x = np.array(df.iloc[:,0:n_cols-1])
+    y = np.array(df.iloc[:,n_cols-1])
+    
+    print(df); print('Dimensions dataset: ', n_cols, '\n\n')
+    
+    
+    
+    #Creamos un dataset donde se guardaran las metricas de cada iteracion
+    df_metrics = pd.DataFrame(columns = ['C', 'epsilon', 'gamma', 'r2', 'RMSE', 'MSE', 'cv', 'time'])
+    
+    for i in range(iter):
+        startTime_iteration = time.perf_counter()
+        folds = KFold(n_splits=k_folds, random_state = None, shuffle=True)
+        #Le indicamos a GridSearchCV la maquina, los parametros y los pliegues
+        sh = GridSearchCV(estimator = estimator_, param_grid = parameters, cv=folds).fit(x, y)
+        #Obtenemos el mejor resultado que el GridSearchCV encontro
+        best_svr = sh.best_estimator_
+    
+    
+        #Hacemos el predict y calculamos las metricas correspondientes
+        y_pred = best_svr.predict(x)
+        r_2 = r2_score(y, y_pred)
+        RMSE = mean_squared_error(y, y_pred, squared=False)
+        MSE = mean_squared_error(y, y_pred)
+    
+        #Contamos e tiempo que toma cada iteracion
+        endTime_iteration = time.perf_counter()
+        
+        #Mostramos en la terminal los resultados de cada iteracion
+        print(['iter:'+str(i), best_svr, 'r2='+str(r_2), 'RMSE='+str(RMSE), 'MSE='+str(MSE),
+               'cv='+str(folds.get_n_splits()), 'time='+str(endTime_iteration-startTime_iteration)])
+        print('###############################################################################################')
+    
+        #Guardamos lo obtenido en un dataset
+        df_metrics.loc[i] = [best_svr.C, best_svr.epsilon, best_svr.gamma, r_2, RMSE, MSE, folds.get_n_splits(), endTime_iteration-startTime_iteration]
+     
+    
+    #Guardamos el dataframe de metricas en un archivo .csv
+    df_metrics.to_csv('metrics_datasets/'+id2save_metrics+'_metrics.csv')
+    
+    #Mostramos el tiempo que tardo el programa
+    print('\nTotal time: {:6.3f} seconds'.format(time.perf_counter() - startTime))
+    
+    #Esta funccion calcula las medias y las errores del dataset de metricas obtenido con la linea 57
+    get_optim_metrics1_ELA(file_to_read= 'metrics_datasets/'+id2save_metrics+'_metrics.csv', 
+                          file_to_create = 'metrics_datasets/'+id2save_metrics+'_metrics_Optim.csv')
--- Regression_comparison/main_SVR_metrics.py
+++ Regression_comparison/main_SVR_metrics.py
@@ -9,8 +9,10 @@ import os; os.system('clear')
 import numpy as np
 from best_SVR_estimator_ELA import best_SVR_estimator_ELA
 from best_Lasso_estimator_ELA import best_Lasso_estimator_ELA
+from best_GBR_estimator_ELA import best_GBR_estimator_ELA
 from sklearn.svm import SVR
 from sklearn.linear_model import Lasso, Ridge
+from sklearn.ensemble import GradientBoostingRegressor
 
 #Diccionario sobre el cual GridSearch buscara los mejores parametros para la SVR
 parameters1_ = {'C': [2**-4, 2**-3, 2**-2, 2**-1, 2**0, 2**1, 2**2, 2**3, 2**4],
@@ -22,12 +24,16 @@ parameters1_ = {'C': [2**-4, 2**-3, 2**-2, 2**-1, 2**0, 2**1, 2**2, 2**3, 2**4],
 #parameters2_ = {'alpha': [.25, .5, .75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5]}
 parameters2_ = {'alpha': np.linspace(.00123, .001235, 30)}
 
+#Diccionario sobre el cual GridSearch buscara los mejores parametros para la GBR
+parameters3_ = {'learning_rate': np.linspace(.01, .1, 10),
+              'n_estimator': np.arange(100,200, 10)}
+
 #Creamos un modelo SVR
 svr = SVR(kernel = 'rbf')
 
 #Creamos un modelo Lasso
 lasso = Lasso()
-ridge = Ridge(max_iter = 50000)
+#)
 
 '''###############################################################################################################
 Esta seccion llama a la función que efectua el GridSearchCV y nos develve el un dataset de los mejores parametros
@@ -41,13 +47,14 @@ Hay que indicar el nombre del dataset a leer y el nombre con el que sera guardad
 
 
 
-
 #Seccion para Lasso (o Ridge)
 best_Lasso_estimator_ELA(dataset_name = 'regression-datasets/housing.csv', id2save_metrics = 'Lasso_housing',
                         estimator_ = lasso, iter = 30, k_folds= 10, parameters = parameters2_)
 
 
-
+#Seccion para GBR
+best_GBR_estimator_ELA(dataset_name = 'regression-datasets/pyrim.csv', id2save_metrics = 'GBR_pyrim',
+                        estimator_ = lasso, iter = 30, k_folds= 10, parameters = parameters3_)
 
 
 
