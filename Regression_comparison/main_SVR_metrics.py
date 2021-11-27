b73f6835518ae4be1ecc0f83173d9dc3b68f4227
Edgar-La <laraae2018@licifug.ugto.mx>
2021-11-27 14:58:54 -0600

    Metrics added

--- Regression_comparison/main_SVR_metrics.py
+++ Regression_comparison/main_SVR_metrics.py
@@ -59,8 +59,6 @@ Hay que indicar el nombre del dataset a leer y el nombre con el que sera guardad
 
 
 #Seccion para GBR
-best_GBR_estimator_ELA(dataset_name = 'regression-datasets/mpg.csv', id2save_metrics = 'GBR_mpg',
-                        estimator_ = gbr, iter = 30, k_folds= 10, parameters = parameters3_)
-
-
+best_GBR_estimator_ELA(dataset_name = 'regression-datasets/mpg.csv', id2save_metrics = 'XGB_mpg',
+                        estimator_ = xgb, iter = 15, k_folds= 5, parameters = parameters3_)
 
